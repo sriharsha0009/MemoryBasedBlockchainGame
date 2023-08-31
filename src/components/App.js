0d4cb6fd39c9a107c2a475a102c3fd4fef2fc345
Sri Harsha Surapaneni <harsha.surapaneni11@gmail.com>
2023-08-31 23:53:05 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3'
 import './App.css';
 import MemoryToken from '../abis/MemoryToken.json'
 import brain from '../brain.png'
+import Identicon from 'identicon.js';
 
 const CARD_ARRAY = [
   {
@@ -182,7 +183,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -191,7 +192,9 @@ class App extends Component {
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+              <small className="text-muted"><span id="account">{this.state.account}</span>
+            
+              </small>
             </li>
           </ul>
         </nav>
